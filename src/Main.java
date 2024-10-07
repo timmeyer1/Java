5ba46cf5c88b893922276d729f2dc0c8405c212f
timmeyer1 <timvameyer@gmail.com>
2024-10-07 11:06:42 +0200

    Ajout dans mainjava

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,15 @@ public class Main {
         int age;
         age = 18;
         long caEntreprise = 25600000L;
+        boolean isExiste = true;
+        char leS = 's';
+        char initiale = '\uf224';
+
 
         System.out.println("Bienvenue " + args[0] + " dans le CFA Java !");
-        System.out.println("Voici une autre ligne " + args[1] );
+        System.out.println("Voici une autre ligne");
+        System.out.println("Vous avez " + age + " ans");
+        System.out.println("unicode: " + initiale);
+        System.out.println("Les variables existent: " + isExiste);
     }
 }

7d4869c863af4d5be64c969487f50be8597b1bba
timmeyer1 <timvameyer@gmail.com>
2024-10-07 13:36:00 +0200

    Collection java

--- src/Collection.java
+++ src/Collection.java
@@ -1,6 +1,25 @@
+// on importe java.util.List pour notre test et java.util.ArrayList
+// ou sinon import java.util.*
+import java.util.ArrayList;
+import java.util.List;
+
 public class Collection {
     public static void main(String[] args) {
-        int[] ages = {5, 15, 18, 22, 55};
+
+        // declarer un tableau d'entier int
+        int[] ages = { 5, 15, 18, 22, 55 };
+
+        // collections
+        List<String> eleves = new ArrayList<String>();
+        eleves.add("Nicolas");
+        eleves.add("Alexandre");
+        eleves.add("Bob Lennon");
+
+        // print simple
+        System.out.println("Age à l'index 2 : " + ages[2]);
+
+        // get permet de récupérer une valeur
+        System.out.println("Eleve à l'index 3 : " + eleves.get(2));
 
     }
 }

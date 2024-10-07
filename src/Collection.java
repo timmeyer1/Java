32746935175aa37d449dfae41a1be891121bc358
timmeyer1 <timvameyer@gmail.com>
2024-10-07 14:05:20 +0200

    Collection done

--- src/Collection.java
+++ src/Collection.java
@@ -1,7 +1,6 @@
 // on importe java.util.List pour notre test et java.util.ArrayList
 // ou sinon import java.util.*
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Collection {
     public static void main(String[] args) {
@@ -13,7 +12,21 @@ public class Collection {
         List<String> eleves = new ArrayList<String>();
         eleves.add("Nicolas");
         eleves.add("Alexandre");
-        eleves.add("Bob Lennon");
+        eleves.add("Mathieu");
+
+        Set<String> villes = new HashSet<>();
+        villes.add("Perpignan");
+        villes.add("Bordeaux");
+        villes.add("Lyon");
+
+        Map<String, String> chiens = new HashMap<>();
+        chiens.put("chien1", "Doberman");
+        chiens.put("chien2", "Husky");
+        chiens.put("chien3", "Golden Retriever");
+        chiens.put("chien4", "Dalmatien");
+
+        // print tableau
+        System.out.println("Les ages sont : " + ages);
 
         // print simple
         System.out.println("Age à l'index 2 : " + ages[2]);
@@ -21,5 +34,13 @@ public class Collection {
         // get permet de récupérer une valeur
         System.out.println("Eleve à l'index 3 : " + eleves.get(2));
 
+        Iterator iterator = villes.iterator();
+        while (iterator.hasNext()) {
+            System.out.println("Voici la ville : " + iterator.next());
+        }
+
+        for(Map.Entry toutou : chiens.entrySet()) {
+            System.out.println("Les chiens sont : " + toutou.getValue() + " " + toutou.getKey());
+        }
     }
 }
